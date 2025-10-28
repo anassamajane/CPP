7e4cd6b7b50a1e6ccb38db3658c1c9da8e6702e1
Anass Amajane <anaamaja@e2r6p10.1337.ma>
2025-10-28 13:07:42 +0100

    ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -4,33 +4,20 @@
 
 int	main(int ac, char **av)
 {
-	int	idx;
-	int	j;
-
-	idx = 0;
-	j = 1;
-	while (j < ac)
-	{
-		idx = 0;
-		while (av[j][idx])
-		{
-			if (islower(av[j][idx]))
-			{
-				av[j][idx] -= 32;
-				std::cout<<(av[j][idx]);
-			}
-			else
-				std::cout<<av[j][idx];
-			idx++;
-		}
-		j++;
-	}
-	if (ac == 1)
-	{
+	if (ac == 1) {
 		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
-		return 1;
+		return 0;
 	}
-	std::cout<<"\n";
+	for (int i = 1; i < ac; i++) {
+		std::string arg = av[i];
+		for (size_t j = 0; j < arg.size(); j++) {
+			if (std::isalpha(arg[j]))
+				arg[j] = std::toupper(arg[j]);
+			std::cout << arg[j];
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }
 
 
