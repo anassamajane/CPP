bb2a906c7bd848500b44e1460b2fdbc395b264a3
anassamajane <anass.amajane2001@gmail.com>
2026-01-05 22:33:25 +0100

    ex02

---
+++ cpp04/ex02/Dog.hpp
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Dog : public Animal
+{
+    private:
+        Brain *brain;
+    public:
+        Dog();
+        Dog(const Dog &other);
+        Dog &operator=(const Dog &other);
+        ~Dog();
+
+        void makeSound() const;
+        Brain *getBrain() const;
+};
+
+#endif
---
+++ cpp04/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include "Brain.hpp"
+
+int main()
+{
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    std::cout << j->getType() << std::endl;
+    std::cout << i->getType() << std::endl;
+
+    j->makeSound();
+    i->makeSound();
+
+    delete j;
+    delete i;
+}
